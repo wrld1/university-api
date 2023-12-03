7e743e22e4761fddf204ff346a6e150f658b2a90
Kyrylo Lolenko <kir260302@gmail.com>
2023-12-04 01:54:35 +0200

    change database config

--- src/configs/database/database-config.ts
+++ src/configs/database/database-config.ts
@@ -14,8 +14,7 @@ export const databaseConfiguration = (
 
   return {
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
+    url: process.env.DATABASE_URL,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
